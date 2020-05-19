fa75593a3ebb0e19c8ea80c835eed426cbdf5a6f
Angelg10 <angelguzman97@gmail.com>
2020-05-19 16:47:10 -0400

    getting friends & map over data

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import Friend from './Friend';
 
 class FriendsList extends React.Component {
     state = {
         friends: []
     };
 
-    // componentDidMount() {
-    //     this.getData();
-    // }
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = () => {
+        axiosWithAuth()
+        .get('/api/friends')
+        .then(res => {
+            console.log(res)
+            // res.data
+            this.setState({
+                friends: res.data
+            })
+        })
+        .catch(err => console.log(err));
+    };
 
     render() {
         return (
             <div>
                 <h2>FRIENDSLIST</h2>
+                {this.state.friends.map(friend => (
+                    <Friend key={friend.id} friend={friend} />
+                ))}
             </div>
         )
     }
